5ecc680657aea90f56c191f4b4a202ceb6a267b8
carpaltunnel <carpaltunnel@carpaltunnel.local>
2026-02-26 17:51:02 -0600

    Echo service

---
+++ server.js
@@ -0,0 +1,22 @@
+import express from 'express';
+const app = express();
+const port = 3000;
+
+// Built in body parser
+app.use(express.json({ limit: '1mb' }));
+
+app.use('/', (req, res) => {
+  console.info(`${req.method} ${req.url} :\n${JSON.stringify(req.body, null, 2)}`);
+  
+  //console.debug(`${new Date().toISOString()} : Returning Date() in JSON Format`);
+  res.json({
+    date: new Date().toISOString(),
+  })
+});
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+}).on('error', (ex) => {
+  console.error(`Server exited abnormally with error : ${ex.message}`);
+});

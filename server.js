810176d971ec3f942ddf170f17606f3798f974c8
carpaltunnel <carpaltunnel@carpaltunnel.local>
2026-03-06 11:34:03 -0600

    Add support for headers

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 import express from 'express';
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 
 // Built in body parser
 app.use(express.json({ limit: '1mb' }));
 
 app.use('/', (req, res) => {
   console.info(`${req.method} ${req.url} :\n${JSON.stringify(req.body, null, 2)}`);
+  console.info('Headers: ');
+  Object.entries(req.headers).forEach((header) => {
+    console.log(`\t${JSON.stringify(header)}`);
+  });
 
   res.json({
     date: new Date().toISOString(),
